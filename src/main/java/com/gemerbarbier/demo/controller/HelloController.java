c300be370cba25717230f6be174e232af59b8901
mmoravcik <marko.moravcik@gmail.com>
2020-06-29 09:35:34 +0200

    cleanup

--- src/main/java/com/gemerbarbier/demo/controller/HelloController.java
+++ src/main/java/com/gemerbarbier/demo/controller/HelloController.java
@@ -5,15 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/")
     public String helloWorld() {
          return "Hello World";
     }
-
-    @GetMapping("/home")
-    public String home() {
-         return "home";
-    }
-
 }
