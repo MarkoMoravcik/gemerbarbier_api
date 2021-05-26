155617f11bd194f09e8b072e93776130998dc05b
MarkoMoravcik <marko.moravcik@gmail.com>
2021-05-26 10:37:46 +0200

    reservation time for lunch updated

--- src/main/java/com/gemerbarbier/config/ReservationTimeConfigConstants.java
+++ src/main/java/com/gemerbarbier/config/ReservationTimeConfigConstants.java
@@ -13,7 +13,7 @@ public class ReservationTimeConfigConstants {
 
     public static final String START_WORKING_TIME = "10:00";
     public static final String START_LUNCH_TIME = "13:00";
-    public static final String END_LUNCH_TIME = "13:00";
+    public static final String END_LUNCH_TIME = "14:00";
     public static final String END_WORKING_TIME = "16:00";
     public static final String END_EXTRA_TIME = "17:00";
 }
