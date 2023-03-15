51f73f4efbfb11cd30ed28a737f17c47817bc3c7
MarkoMoravcik <marko.moravcik@gmail.com>
2023-03-15 18:07:53 +0100

    Check reservation exists functionality added

--- src/main/java/sk/gemerbarbier/service/ReservationService.java
+++ src/main/java/sk/gemerbarbier/service/ReservationService.java
@@ -1,34 +1,35 @@
 package com.gemerbarbier.service;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
-import java.util.Optional;
-
-import javax.validation.constraints.NotNull;
-
 import com.gemerbarbier.config.ReservationCutConfigEnum;
 import com.gemerbarbier.config.ReservationTimeConfigConstants;
 import com.gemerbarbier.data.Reservation;
 import com.gemerbarbier.data.ReservationDates;
 import com.gemerbarbier.data.ReservationTime;
+import com.gemerbarbier.exception.CreateReservationException;
 import com.gemerbarbier.repository.ReservationDatesRepository;
 import com.gemerbarbier.repository.ReservationRepository;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+import java.util.Optional;
+import javax.validation.constraints.NotNull;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class ReservationService {
 
-  @Autowired
-  private ReservationRepository repository;
-
-  @Autowired
-  private ReservationDatesRepository datesRepository;
+  private final ReservationRepository reservationRepository;
+  private final ReservationDatesRepository datesRepository;
 
   public void createReservation(Reservation reservation) {
-
+    Optional<ReservationDates> optReservationDate = datesRepository.findByDateAndBarber(
+        reservation.getDate(), reservation.getBarber());
+    if (optReservationDate.isEmpty() || !reservationTimeIsPossible(optReservationDate.get(),
+        reservation.getTime())) {
+      throw new CreateReservationException(
+          "Reservation was not possible to create. Reservation date does not exist or reservation time is already booked.");
+    }
     LocalTime parsedTime = LocalTime.parse(reservation.getTime());
     String parsedDate = LocalDate.parse(reservation.getDate()).toString();
     ReservationCutConfigEnum cutType = ReservationCutConfigEnum.valueOf(reservation.getCutTag());
@@ -38,35 +39,42 @@ public class ReservationService {
     reservation.setColor(cutType.getCutColor());
     reservation.setCutType(cutType.getCutName());
 
-    repository.save(reservation);
+    reservationRepository.save(reservation);
   }
 
   public List<Reservation> getReservations(String barber) {
-    return repository.findByBarber(barber);
+    return reservationRepository.findByBarber(barber);
+  }
+
+  public void deleteReservation(@NotNull Long id, @NotNull String barber) {
+    Optional<Reservation> optReservation = reservationRepository.findByIdAndBarber(id, barber);
+    if (optReservation.isPresent()) {
+      Optional<ReservationDates> optDate = datesRepository.findByDateAndBarber(
+          optReservation.get().getDate(), barber);
+      if (optDate.isPresent()) {
+        Optional<ReservationTime> optTime = optDate.get().getAvailableTimes().stream()
+            .filter(t -> t.getTime().equals(optReservation.get().getTime())).findFirst();
+        if (optTime.isPresent()) {
+          updateTimes(optReservation.get(), optDate.get());
+          datesRepository.save(optDate.get());
+        }
+      }
+      reservationRepository.deleteById(id);
+    }
+  }
+
+  private boolean reservationTimeIsPossible(ReservationDates reservationDate,
+      String reservationTime) {
+    return reservationDate.getAvailableTimes().stream()
+        .filter(time -> time.getState().equals("Active")).map(ReservationTime::getTime).toList()
+        .contains(reservationTime);
   }
 
   private String createDateTime(String parsedDate, LocalTime parsedTime, Long plusMinutes) {
     return parsedDate + " " + parsedTime.plusMinutes(plusMinutes).toString();
   }
 
-  public void deleteReservation(@NotNull Long id, @NotNull String barber) {
-    Optional<Reservation> optReservation = repository.findByIdAndBarber(id, barber);
-    if (optReservation.isPresent()) {
-      Optional<ReservationDates> optDate = datesRepository.findByDateAndBarber(optReservation.get().getDate(), barber);
-      if (optDate.isPresent()) {
-        Optional<ReservationTime> optTime = optDate.get().getAvailableTimes().stream()
-            .filter(t -> t.getTime().equals(optReservation.get().getTime())).findFirst();
-        if (optTime.isPresent()) {
-          updateTimes(optReservation.get(), optDate.get(), optTime.get());
-          datesRepository.save(optDate.get());
-        }
-      }
-      repository.deleteById(id);
-    }
-  }
-
-  private void updateTimes(Reservation reservation, ReservationDates dates,
-      ReservationTime reservationTime) {
+  private void updateTimes(Reservation reservation, ReservationDates dates) {
     ReservationCutConfigEnum cutType = ReservationCutConfigEnum.valueOf(reservation.getCutTag());
     LocalTime parsedTime = LocalTime.parse(reservation.getTime());
     Long cutTime = cutType.getCutTime();
