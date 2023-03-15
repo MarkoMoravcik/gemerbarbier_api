89319be244dec245666022fe2aca0b6d3d8a6781
MarkoMoravcik <marko.moravcik@gmail.com>
2023-03-15 18:17:31 +0100

    Serial annotation removed

--- src/main/java/sk/gemerbarbier/exception/CreateReservationException.java
+++ src/main/java/sk/gemerbarbier/exception/CreateReservationException.java
@@ -1,10 +1,7 @@
 package com.gemerbarbier.exception;
 
-import java.io.Serial;
-
 public class CreateReservationException extends RuntimeException {
 
-  @Serial
   private static final long serialVersionUID = 2194523997557273025L;
 
   public CreateReservationException(final String message) {
